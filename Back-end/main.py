717383f7d73ab50b395a13d7b79bae65a5770387
abolfazmz81 <abolfazmz81@gmail.com>
2024-07-04 01:35:58 +0330

    feat(Back-end): Add Signup Api

--- Back-end/main.py
+++ Back-end/main.py
@@ -1,3 +1,4 @@
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from Models import *
@@ -5,7 +6,7 @@ from sqlalchemy import desc
 from database import SessionLocal, engine, Base, get_db
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated, ClassVar, List
-from Schemes.test import input
+from Schemes import *
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
@@ -22,9 +23,18 @@ app.add_middleware(
 db_dependency = Annotated[Session, Depends(get_db())]
 
 
-@app.post("/scores")
-async def update_score(json:input, db: Session = Depends(get_db)):
-    return input.name
+@app.post("/Signup")
+async def update_score(json:SignUp, db: Session = Depends(get_db)):
+    check = db.query(Owner).filter(Owner.nationalId == json.nationalCode).first()
+    check2 = db.query(Owner).filter(Owner.number == json.phoneNumber).first()
+    if check or check2:
+        return JSONResponse(content="the user with this national id or phone number already exists",status_code=400)
+    nuser = Owner(Oname=json.name,Olname=json.lname,number=json.phoneNumber,password=json.password,nationalId=json.nationalCode)
+    db.add(nuser)
+    db.commit()
+    db.refresh(nuser)
+    return JSONResponse(content="ok",status_code=200)
+
 
 
 @app.get("/top")
